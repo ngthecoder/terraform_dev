1019da55b35a3b8eb562c60e7da1006b19192840
ngthecoder <ngoto0208@gmail.com>
2024-06-08 00:25:26 +0900

    applied terraform fmt

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.123.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -8,21 +8,21 @@ resource "aws_vpc" "dev_vpc" {
   }
 }
 
-resource "aws_subnet" "dev_subnet"{
-    vpc_id = aws_vpc.dev_vpc.id
-    cidr_block = "10.123.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-east-1a"
+resource "aws_subnet" "dev_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 
-    tags = {
-        Name = "dev-public"
-    }
+  tags = {
+    Name = "dev-public"
+  }
 }
 
 resource "aws_internet_gateway" "dev_igw" {
-    vpc_id = aws_vpc.dev_vpc.id
+  vpc_id = aws_vpc.dev_vpc.id
 
-    tags = {
-        Name = "dev-igw"
-    }
+  tags = {
+    Name = "dev-igw"
+  }
 }
