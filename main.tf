3280d4433d0554c5645f7684530adea64c17f5a0
ngthecoder <ngoto0208@gmail.com>
2024-06-08 00:22:50 +0900

    added subnet and internet gateway

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "mtc_vpc" {
+resource "aws_vpc" "dev_vpc" {
   cidr_block = "10.123.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
@@ -6,4 +6,23 @@ resource "aws_vpc" "mtc_vpc" {
   tags = {
     Name = "dev"
   }
+}
+
+resource "aws_subnet" "dev_subnet"{
+    vpc_id = aws_vpc.dev_vpc.id
+    cidr_block = "10.123.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "dev-public"
+    }
+}
+
+resource "aws_internet_gateway" "dev_igw" {
+    vpc_id = aws_vpc.dev_vpc.id
+
+    tags = {
+        Name = "dev-igw"
+    }
 }
