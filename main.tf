73b21a5f1c94b71578fbc927c0be58c2f0b37829
ngthecoder <ngoto0208@gmail.com>
2024-06-11 19:55:41 +0900

    added key pair and ec2 instance

--- main.tf
+++ main.tf
@@ -64,3 +64,25 @@ resource "aws_security_group" "dev_sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_key_pair" "dev_auth" {
+    key_name = "dev_key"
+    public_key = file("${var.ssh_path}")
+}
+
+resource "aws_instance" "dev_node" {
+    instance_type = "t2.micro"
+    ami = data.aws_ami.server_ami.id
+    key_name = aws_key_pair.dev_auth.key_name
+    vpc_security_group_ids = [aws_security_group.dev_sg.id]
+    subnet_id = aws_subnet.dev_subnet.id
+    user_data = file("userdata.tpl")
+
+    root_block_device {
+        volume_size = 10
+    }
+
+    tags = {
+        Name = "dev-node"
+    }
+}
