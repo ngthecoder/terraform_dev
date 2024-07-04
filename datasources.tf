f335629341ded74d75fe81f6680866e4660a57dc
ngthecoder <ngoto0208@gmail.com>
2024-07-04 21:12:15 +0900

    changed AMI to fix bug

--- datasources.tf
+++ datasources.tf
@@ -4,6 +4,6 @@ data "aws_ami" "server_ami" {
 
     filter {
         name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
     }
 }
