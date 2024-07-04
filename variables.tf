631bcdf3e4582ee65b60a7c494df0391b9a8afbb
ngthecoder <ngoto0208@gmail.com>
2024-07-04 21:08:01 +0900

    added host os and ssh public key path

--- variables.tf
+++ variables.tf
@@ -1,9 +1,20 @@
+variable "host_os" {
+    description = "Host OS (linux or windows)"
+    type = string
+    default = "linux"
+}
+
 variable "home_ip" {
     description = "Home IP address"
     type = string
 }
 
-variable "ssh_path" {
-    description = "Path to SSH key"
+variable "ssh_public_path" {
+    description = "Path to SSH Public Key"
+    type = string
+}
+
+variable "ssh_private_path" {
+    description = "Path to SSH Private Key"
     type = string
 }
